f9bcb53e37753e0961c0294e2b4d4fa48d314180
Kirilloao <kirilltaraturin747@gmail>
2023-09-18 13:06:53 +0200

    Initial Commit

---
+++ KirillCard/KirillCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  KirillCardApp.swift
+//  KirillCard
+//
+//  Created by Kirill Taraturin on 18.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct KirillCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
