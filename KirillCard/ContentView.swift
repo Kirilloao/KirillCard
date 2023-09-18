054d7d80393e29ffbd6510595eab22805f70ad01
Kirilloao <kirilltaraturin747@gmail>
2023-09-18 15:00:12 +0200

    done

--- KirillCard/ContentView.swift
+++ KirillCard/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52).ignoresSafeArea()
+            VStack {
+                Image("me")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150, height: 200)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 5)
+                    )
+                Text("Kirill Taraturin")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                
+                
+                InfoView(text: "+36 240 452 04 68", imageName: "phone.fill")
+                InfoView(text: "kirilltaraturin747@gmail.com", imageName: "envelope.fill")
+            }
         }
-        .padding()
     }
 }
 
@@ -24,3 +42,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
---
+++ KirillCard/InfoView.swift
@@ -0,0 +1,32 @@
+//
+//  InfoView.swift
+//  KirillCard
+//
+//  Created by Kirill Taraturin on 18.09.2023.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay(HStack {
+                Image(systemName: imageName).foregroundColor(.green)
+                Text(text)
+            })
+            .padding(.all)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "123", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
